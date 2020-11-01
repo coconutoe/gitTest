0f2b8f429fd32e38ed723f3ea01ce73d05da150a
coconutoe <694896471@qq.com>
2020-11-01 12:34:39 +0800

    add files

---
+++ src/main/java/com/atguigu/maven/Hello.java
@@ -0,0 +1,7 @@
+package com.atguigu.maven;
+
+public class Hello {
+	public void sayHello() {
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/atguigu/maven/HelloTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.maven;
+
+import org.junit.Test;
+
+public class HelloTest {
+	@Test
+	public void testHello() {
+		Hello he = new Hello();
+		
+		he.sayHello();
+	}
+	
+	@Test
+	public void testHello2() {
+		System.out.println("hello, xiaohuihui");
+	}
+}
